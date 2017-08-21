ffc6f230172928fff91a1036592ea4a58d4d006e
prabhu-durasoft <iamprabhu@yahoo.com>
2017-08-21 11:41:01 +0530

    Lab02

---
+++ Day01/arrays.js
@@ -0,0 +1,5 @@
+var numbers = [];
+var even = [2,4,6,8];
+for(var i=0;i<even.length;i++){
+    console.log(even[i])
+}
---
+++ Day01/helloworld.js
@@ -0,0 +1,19 @@
+//JavaScript is interpreted
+//Case sensitive
+//Loops, conditions similar to C/Java
+//Semi-colon is optional
+//dynamically typed language
+
+var i = 10;
+var s = "cool";
+s = true;
+s = 'string in single quotes';
+s = 23.456;
+
+var j;
+if(j != undefined)
+    console.log(j)
+else
+    console.log("j is undefined")
+
+console.log("Hello JS")
